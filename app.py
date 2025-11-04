b114e56ad9ac224791119cde05ec5f5e27912be9
roysmb77 <157696708+roysmb77@users.noreply.github.com>
2025-11-04 14:32:46 +0700

    Update app.py

--- app.py
+++ app.py
@@ -48,5 +48,7 @@ def predict():
         return redirect(url_for('index'))
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8080)) app.run(host="0.0.0.0", port=port)
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host="0.0.0.0", port=port)
+
 
